b7eb5773ca040871095e727d417eac3ee24de3ea
Mostafa Ayman <moaymn707@gmail.com>
2026-02-26 03:59:55 +0200

    feat: implement animation variants for main content and animated blobs

--- src/App.jsx
+++ src/App.jsx
@@ -40,12 +40,59 @@ function App() {
     return () => clearInterval(timer);
   }, [])
   
+  const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: "easeOut",
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: {
+    opacity: 0,
+    y: 40,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  },
+};
   
 
   return (
     <>
       <div className="relative min-h-screen flex justify-center items-center bg-[#0a0a0a] text-white p-4 md:py-14 font-mono overflow-hidden">
-        <h1 className="text-white text-8xl font-bold">STARTER CODE</h1>
+        {animatedBlobs.map((blob, i)=> (
+          <motion.div key={i}
+          className={`absolute rounded-full ${blob.className}`}
+          animate = {blob.animate}
+          transition={{
+            duration: blob.duration,
+            ease: "easeInOut", 
+            repeat: Infinity,
+            repeatType: "mirror"
+          }}
+          ></motion.div>
+        ))}
+
+        <motion.main
+          variants={containerVariants} 
+          initial="hidden"
+          animate={mounted? "show" : "hidden"}
+          className="main-grid"
+        ></motion.main>
+
       </div>
     </>
   );
